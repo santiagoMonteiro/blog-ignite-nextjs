a0f6f02da6dd0ff1f9518f46171cf17315094a62
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2022-03-31 16:55:49 -0400

    header construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import Head from "next/head";
 import { GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,13 +26,35 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Home | Spacetraveling</title>
+      </Head>
+      
+      <h1>Hello World!</h1>
+    </>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      pageSize: 1,
+    }
+  );
 
-//   // TODO
-// };
+  const postsPagination: PostPagination = {
+    next_page: postsResponse.next_page,
+    results: postsResponse.results,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
